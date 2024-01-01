7119d22cb9f0e2fbce12d81df63fb47ec4fa455b
mazharul-islam321 <mazharulislam.lu2016@gmail.com>
2024-01-02 04:31:34 +0600

    added semester registration crud

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { semesterRegistrationFilterableFields } from './semesterRegistration.constants';
 import { SemesterRegistrationService } from './semesterRegistration.service';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
@@ -15,8 +18,66 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
     });
 });
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, semesterRegistrationFilterableFields);
+    const options = pick(req.query, paginationFields);
+    const result = await SemesterRegistrationService.getAllFromDB(
+        filters,
+        options
+    );
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'SemesterRegistrations fetched successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await SemesterRegistrationService.getByIdFromDB(id);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'SemesterRegistration fetched successfully',
+        data: result,
+    });
+});
+
+const updateOneInDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await SemesterRegistrationService.updateOneInDB(
+        id,
+        req.body
+    );
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'SemesterRegistration updated successfully',
+        data: result,
+    });
+});
+
+const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await SemesterRegistrationService.deleteByIdFromDB(id);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'SemesterRegistration deleted successfully',
+        data: result,
+    });
+});
+
 export const SemesterRegistrationController = {
     insertIntoDB,
-    // getAllFromDB,
-    // getByIdFromDB,
+    getAllFromDB,
+    getByIdFromDB,
+    updateOneInDB,
+    deleteByIdFromDB,
 };
--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -7,13 +7,28 @@ import { SemesterRegistrationValidation } from './semesterRegistration.validatio
 
 const router = express.Router();
 
-// router.get('/', SemesterRegistrationController.getAllFromDB);
+router.get('/', SemesterRegistrationController.getAllFromDB);
+
+router.get('/:id', SemesterRegistrationController.getByIdFromDB);
 
 router.post(
     '/create-semester-reg',
     validateRequest(SemesterRegistrationValidation.create),
     // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
     SemesterRegistrationController.insertIntoDB
+);
+
+router.patch(
+    '/:id',
+    validateRequest(SemesterRegistrationValidation.update),
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    SemesterRegistrationController.updateOneInDB
+);
+
+router.delete(
+    '/:id',
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    SemesterRegistrationController.deleteByIdFromDB
 );
 
 export const semesterRegistrationRoutes = router;
